1b6cb5b7f82a6e561f50eca3348453895762f2fa
tkz96 <tkz9696@gmail.com>
2022-10-21 08:59:29 -0700

    Rename user_id to author_id

--- db/migrate/20221021151859_add_user_ref_post_ref_to_comments.rb
+++ db/migrate/20221021151859_add_user_ref_post_ref_to_comments.rb
@@ -1,6 +1,5 @@
 class AddUserRefPostRefToComments < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :user, null: false, foreign_key: true
     add_reference :comments, :post, null: false, foreign_key: true
   end
 end
