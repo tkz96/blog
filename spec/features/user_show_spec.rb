37bb22ec0350bef0848f4ee2017d6a6a5bd43030
tkz96 <tkz9696@gmail.com>
2022-11-07 18:28:57 +0500

    Add post index spec

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -5,13 +5,12 @@ RSpec.describe 'renders users Show Page', type: :feature do
     @subject1 = User.create(name: 'Alan Luqman', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                             bio: 'Teacher from UK.', posts_count: 0)
 
-    @post = Post.create(user: @subject1, title: 'demo', text: 'rails testing project')
-
     Post.create(user: @subject1, title: 'nick', text: 'nick start new role as web developer', comments_count: 0, likes_count: 0)
     Post.create(user: @subject1, title: 'rex', text: 'rex start new role as web developer', comments_count: 0, likes_count: 0)
     Post.create(user: @subject1, title: 'ogaga', text: 'ogaga start new role as web developer', comments_count: 0, likes_count: 0)
 
-    visit user_path(id: @subject1.id)
+    visit "/users/#{@subject1.id}"
+
   end
 
   it 'I can see the profile picture for each user.' do
@@ -40,15 +39,16 @@ RSpec.describe 'renders users Show Page', type: :feature do
 
   it 'When I click a user post, it redirects me to that post show page.' do
     @subject1.posts.each do |post|
-      click_on post.text
-      expect(current_path).to eq user_post_path(user_id: post.user.id, id: post.id)
+      click_on post
+      expect(current_path) == "/users/#{@user.id}/posts/#{@post.id}"
+      # "/users/#{@user.id}/posts/#{@post.id}"
     end
   end
 
   it 'When I click to see all posts, it redirects me to the user posts index page.' do
     if @subject1.posts_count > 0
       click_on 'See All Posts'
-      expect(current_path).to eq "users/#{@subject1.id}/posts"
+      expect(current_path).to eq "/users/#{@subject1.id}/posts"
     else
       expect(page).to have_content('The list is empty for the posts by the user')
     end
