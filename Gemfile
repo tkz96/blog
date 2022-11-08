4684c76dc757d671970618bf87635022f96d803f
tkz96 <tkz9696@gmail.com>
2022-11-08 20:25:21 +0500

    Add devisse

--- Gemfile
+++ Gemfile
@@ -6,8 +6,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.1.2'
 
 gem 'bootsnap', require: false
+gem 'devise'
 gem 'importmap-rails'
 gem 'jbuilder'
+gem 'letter_opener'
 gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
 gem 'rails', '~> 7.0.4'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,23 @@
 
 # The user class
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy, foreign_key: :user_id
   accepts_nested_attributes_for :posts
+  before_create :default_posts_count
 
   has_many :comments
   has_many :likes
 
   validates :name, presence: true
-  validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def default_posts_count
+    self.posts_count = 0
+  end
 
   def return_recent_posts
     posts.order(created_at: :desc).limit(3)
